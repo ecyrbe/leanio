ce769be3c219186bcac82fe321f286e9c8e3e7b7
ecyrbe <ecyrbe@gmail.com>
2026-06-26 23:28:19 +0200

    fix: use proper lemma for String

--- Leanio/Data/String.lean
+++ Leanio/Data/String.lean
@@ -2,7 +2,7 @@ namespace String
 
 private theorem find?_eq_some_imp_ne_endPos {s : String} {c : Char} {pos : s.Pos}
     (h : s.find? c = some pos) : pos ≠ s.endPos := by
-  exact (String.find?_bool_eq_some_iff.mp h).1
+  exact (String.find?_char_eq_some_iff.mp h).1
 
 def splitOnce (s : String) (pat : Char) : Option (String × String) :=
   match h: s.find? pat with
