09c3fa78ec2ef7e85095439843880fb750044270
ecyrbe <ecyrbe@gmail.com>
2026-07-04 15:09:57 +0200

    feat: add top level mime type helper

--- LeanIO/Data/MimeType.lean
+++ LeanIO/Data/MimeType.lean
@@ -3,30 +3,59 @@ import Std.Http
 namespace LeanIO.MimeType
 open Std.Http
 
-def octetStream    : Header.Value := Header.Value.mk "application/octet-stream"
-def textPlain      : Header.Value := Header.Value.mk "text/plain"
-def textHtml       : Header.Value := Header.Value.mk "text/html"
-def textCss        : Header.Value := Header.Value.mk "text/css"
-def applicationJavascript  : Header.Value := Header.Value.mk "application/javascript"
-def applicationJson        : Header.Value := Header.Value.mk "application/json"
-def applicationPdf         : Header.Value := Header.Value.mk "application/pdf"
-def imagePng       : Header.Value := Header.Value.mk "image/png"
-def imageJpeg      : Header.Value := Header.Value.mk "image/jpeg"
-def imageGif       : Header.Value := Header.Value.mk "image/gif"
-def imageSvg       : Header.Value := Header.Value.mk "image/svg+xml"
-def imageWebp      : Header.Value := Header.Value.mk "image/webp"
-def imageIcon      : Header.Value := Header.Value.mk "image/x-icon"
-def videoMp4       : Header.Value := Header.Value.mk "video/mp4"
-def videoWebm      : Header.Value := Header.Value.mk "video/webm"
-def videoOgg       : Header.Value := Header.Value.mk "video/ogg"
-def videoQuicktime : Header.Value := Header.Value.mk "video/quicktime"
-def videoAvi       : Header.Value := Header.Value.mk "video/x-msvideo"
-def videoMkv       : Header.Value := Header.Value.mk "video/x-matroska"
-def audioMpeg      : Header.Value := Header.Value.mk "audio/mpeg"
-def audioWav       : Header.Value := Header.Value.mk "audio/wav"
-def audioFlac      : Header.Value := Header.Value.mk "audio/flac"
-def formUrlEncoded : Header.Value := Header.Value.mk "application/x-www-form-urlencoded"
-def multipartForm  : Header.Value := Header.Value.mk "multipart/form-data"
+inductive TopLevel
+  | application
+  | audio
+  | font
+  | haptics
+  | image
+  | message
+  | model
+  | multipart
+  | text
+  | video
+
+instance : ToString TopLevel where
+  toString lvl := match lvl with
+  | .application => "application"
+  | .audio => "audio"
+  | .font => "font"
+  | .haptics => "haptics"
+  | .image => "image"
+  | .message => "message"
+  | .model => "model"
+  | .multipart => "multipart"
+  | .text => "text"
+  | .video => "video"
+
+
+def octetStream    := Header.Value.mk "application/octet-stream"
+def textPlain      := Header.Value.mk "text/plain"
+def textHtml       := Header.Value.mk "text/html"
+def textCss        := Header.Value.mk "text/css"
+def textMarkdown   := Header.Value.mk "text/markdown"
+def textJavascript := Header.Value.mk "text/javascript"
+def imagePng       := Header.Value.mk "image/png"
+def imageJpeg      := Header.Value.mk "image/jpeg"
+def imageGif       := Header.Value.mk "image/gif"
+def imageSvg       := Header.Value.mk "image/svg+xml"
+def imageWebp      := Header.Value.mk "image/webp"
+def imageIcon      := Header.Value.mk "image/x-icon"
+def videoMp4       := Header.Value.mk "video/mp4"
+def videoWebm      := Header.Value.mk "video/webm"
+def videoOgg       := Header.Value.mk "video/ogg"
+def videoQuicktime := Header.Value.mk "video/quicktime"
+def videoAvi       := Header.Value.mk "video/x-msvideo"
+def videoMkv       := Header.Value.mk "video/x-matroska"
+def audioMpeg      := Header.Value.mk "audio/mpeg"
+def audioWav       := Header.Value.mk "audio/wav"
+def audioFlac      := Header.Value.mk "audio/flac"
+def formUrlEncoded := Header.Value.mk "application/x-www-form-urlencoded"
+def multipartForm  := Header.Value.mk "multipart/form-data"
+def applicationJs  := Header.Value.mk "application/javascript"
+def applicationJson:= Header.Value.mk "application/json"
+def applicationPdf := Header.Value.mk "application/pdf"
+def applicationZip := Header.Value.mk "application/zip"
 
 end LeanIO.MimeType
 
@@ -35,6 +64,11 @@ namespace Std.Http.Headers
 def hasMimeType (self : Headers) (mimeType : Header.Value) : Bool :=
   match self.get? .contentType with
   |some contentType => contentType.value.startsWith mimeType.value
+  |none => false
+
+def hasMimeTopLevel (self : Headers) (level: LeanIO.MimeType.TopLevel): Bool :=
+  match self.get? .contentType with
+  |some contentType => contentType.value.startsWith s!"{level}/"
   |none => false
 
 end Std.Http.Headers
